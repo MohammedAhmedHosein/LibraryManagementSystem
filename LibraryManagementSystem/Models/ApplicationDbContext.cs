c34662e686e4f875f85013cd2495db914f45241e
MohammedAhmedHosein <m.ahmedhosein67@gmail.com>
2024-08-17 16:15:11 +0300

    Setup ApplicationAdd project files.

---
+++ LibraryManagementSystem/Models/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base( options) { }
+
+        public DbSet<User> Users { get; set; }  // Change to public DbSet<User> Users { get; set; }
+    }
+}
---
+++ LibraryManagementSystem/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementSystem.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        public string PasswordHash { get; set; } // Store hashed passwords
+
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        public string Role { get; set; } // Role (e.g., 'admin', 'librarian', 'member')
+
+    }
+}
